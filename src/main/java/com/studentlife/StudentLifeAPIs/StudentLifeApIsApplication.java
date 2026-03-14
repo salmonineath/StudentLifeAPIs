e2d21e9b6d29273d38f165e2311e1635f4880f50
Your Name <your.email@example.com>
2026-03-14 22:24:59 +0700

    first commit init project

---
+++ src/main/java/com/studentlife/StudentLifeAPIs/StudentLifeApIsApplication.java
@@ -0,0 +1,13 @@
+package com.studentlife.StudentLifeAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLifeApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLifeApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentlife/StudentLifeAPIs/StudentLifeApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentlife.StudentLifeAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLifeApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
