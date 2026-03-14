e6e20477482e2b6eb2dfa504947976ad6a33f27c
Your Name <your.email@example.com>
2026-03-14 23:29:14 +0700

    add docker container

--- src/main/java/com/studentlife/StudentLifeAPIs/StudentLifeApIsApplication.java
+++ src/main/java/com/studentlife/StudentLifeAPIs/StudentLifeApIsApplication.java
@@ -8,6 +8,7 @@ public class StudentLifeApIsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentLifeApIsApplication.class, args);
+		System.out.println("Application is running!");
 	}
 
 }
